bd9ecb5c6a8d14c0fa38c4db3be397bd7958927e
john dowd and roger tobias <john@>
2017-05-01 11:54:56 -0700

    simpfied the code with forEach function dry method

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    //
+    // $(".person1").append(person1Input);
+    // $(".person2").append(person2Input);
+    // $(".animal").append(animalInput);
+    // $(".exclamation").append(exclamationInput);
+    // $(".verb").append(verbInput);
+    // $(".noun").append(nounInput);
 
     $("#story").show();
 
